8e0432545ed1299aed6cf032a4fac6ac4ae4278b
EKIZAMANI <eki.zamani@gmail.com>
2023-04-08 13:12:00 +0700

    feat: add assets

---
+++ CapyType/resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite("resources/css/app.css")
+
+</head>
+<body class="aspect-video" style="background-color: 262A33;">
+    <h1 class="p-2">text 1</h1>
+    <p class="m-5 w-10">text 1</p>
+</body>
+</html>
