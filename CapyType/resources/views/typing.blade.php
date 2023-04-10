701bce324e4def879568c0413b22c91f4be808eb
Fanul1 <fanul@mhs.unsyiah.ac.id>
2023-04-10 12:18:42 +0700

    feat: add typing page

---
+++ CapyType/resources/views/typing.blade.php
@@ -0,0 +1,100 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Typing Page</title>
+    @vite('resources/css/app.css')
+
+</head>
+
+<body class="font-mono text-xl aspect-video" style="background-color: #262A33;" width="1920" height="1080">
+    <!-- awal navbar -->
+    <div class="flex flex-wrap items-center justify-between mx-auto " style="padding-top: 30px;">
+        <div class="flex items-center ml-10"> <img src="{{ asset('assets/img/logocapy.png') }}" class="mr-3" alt="Icon"
+                width="65" />
+            <span class="self-center text-3xl font-bold"
+                style="font-size: 40px; color: 
+                #E5F7EF;">CapyType</span>
+            <div class="flex items-center pl-10">
+                <a href=""><img src="{{ asset('assets/img/keyboard.png') }}" class="pr-2 mr-3" alt="Icon"
+                        width="54" /></a>
+                <a href=""><img src="{{ asset('assets/img/crown.png') }}" class="pr-2 mr-3"alt="Icon"
+                        width="45" /></a>
+                <a href=""><img src="{{ asset('assets/img/information.png') }}" class="pr-2 mr-3" alt="Icon"
+                        width="35" /></a>
+                <a href=""><img src="{{ asset('assets/img/setting.png') }}" class="pr-2 mr-3" alt="Icon"
+                        width="35" /></a>
+            </div>
+        </div>
+
+        <a href=""  class="px-5 py-3 mr-10 font-bold rounded-lg"
+            style="background-color: #1F232C; font-size: 16px; color: #526777;">register/login</a>
+    </div>
+    <!-- akhir navbar -->
+
+    <!-- awal typing section -->
+    <div class="flex justify-center mx-20 my-10">
+        <div class="flex items-center px-8 py-2 font-mono text-base font-medium text-justify text-gray-700 bg-gray-900 rounded-lg">
+            <button>#custom-words</button>
+            <button>
+            <div class="flex items-center"><img src="{{ asset('assets/img/clock.png') }}" alt="time" class="ml-4" width="22" height="23" style="filter: invert(62%) sepia(49%) saturate(637%) hue-rotate(107deg) brightness(90%) contrast(82%);">
+                <span class="ml-2" style="color: #2ABA86">time</span>
+            </div></button>
+        <button>
+            <div class="flex items-center"><img src="{{ asset('assets/img/A.png') }}" alt="A" class="ml-4" width="22" height="20" style="filter: invert(38%) sepia(13%) saturate(808%) hue-rotate(164deg) brightness(95%) contrast(85%);">
+                <span class="ml-2">words</span></button>
+            <div class="flex items-center ml-4">
+                <button class="ml-2">15</button>
+                <button class="ml-2">30</button>
+                <button class="ml-2" style="color: #2ABA86">60</button>
+                <button class="ml-2">120</button>
+            </div>
+        </div>
+    </div>
+
+    <button class="flex items-center justify-center mx-auto">
+        <img src="{{ asset('assets/img/earth.png') }}" alt="" class="mr-2" width="18" height="16" style="filter: invert(38%) sepia(13%) saturate(808%) hue-rotate(164deg) brightness(95%) contrast(85%);">
+        <span class="ml-1 font-mono text-sm font-medium text-gray-600" >Indonesia</span>
+      </button>
+    
+    <div class="flex justify-center mx-20 my-16">
+        <span class="text-2xl text-center">
+            <span class="text-gray-500">astaga asal guna setelah mencoba aku mengatakan tanaman kemudian karena benat </span>sedangkan sungguh habis walaupun bahwa amarah terlalu wajah layangan jarak nama cari rasio macam sekolah kemarin akar saya bahkan menjadi terbang takut hilang
+        </span>
+    </div>
+    <div class="flex justify-center mx-20 my-16">
+        <button type="reset"><img src="{{ asset('assets/img/refresh-arrow.png') }}" alt="" style="filter: invert(62%) sepia(49%) saturate(637%) hue-rotate(107deg) brightness(90%) contrast(82%);" width="35" height="35"></button>
+    </div>
+    <!-- akhir typing section -->
+
+    <!-- awal footer -->
+    <footer class="fixed bottom-0 ml-10" style="font-size:20; width:900;" >
+        <div class="flex flex-wrap p-3" style="background-color: #262A33;">
+            <a href="" class="flex items-center justify-center">
+                <img src="{{ asset('assets/img/envelope.png') }}" alt="Icon" class="pr-2" width="37" style="filter: invert(38%) sepia(13%) saturate(808%) hue-rotate(164deg) brightness(95%) contrast(85%);">
+                <p class="mr-2 text-gray-500">contact</p>
+            </a>
+            <a href="" class="flex items-center justify-center"><img src="{{ asset('assets/img/discord.png') }}" alt="Icon"
+                    class="pr-2" width="35" style="filter: invert(38%) sepia(13%) saturate(808%) hue-rotate(164deg) brightness(95%) contrast(85%);">
+                <p class="mr-2 text-gray-500">discord</p>
+            </a>
+
+            <a href="" class="flex items-center justify-center"><img src="{{ asset('assets/img/telegram.png') }}" alt="Icon"
+                    class="pr-2" width="30" style="filter: invert(38%) sepia(13%) saturate(808%) hue-rotate(164deg) brightness(95%) contrast(85%);">
+                <p class="mr-2 text-gray-500">telegram</p>
+            </a>
+
+            <a href="" class="flex items-center justify-center"><img src="{{ asset('assets/img/money.png') }}" alt="Icon"
+                    class="pr-2" width="30" style="filter: invert(38%) sepia(13%) saturate(808%) hue-rotate(164deg) brightness(95%) contrast(85%);">
+                <p class="mr-2 text-gray-500">support</p>
+            </a>
+
+        </div>
+    </footer>
+
+    <!-- akhir footer -->
+</body>
+
+</html>
