cb7028ece10de3b3408d78d778b20ac449b4c544
Fanul1 <fanul@mhs.unsyiah.ac.id>
2023-05-02 14:57:49 +0700

    feat: add typing time

--- CapyType/resources/views/typing.blade.php
+++ CapyType/resources/views/typing.blade.php
@@ -17,7 +17,7 @@
     <!-- awal typing section -->
     <div class="flex justify-center mx-20 my-10">
         <div
-            class="flex items-center px-8 py-2 font-mono text-base font-medium text-justify text-gray-700 bg-gray-900 rounded-lg">
+            class="flex items-center px-8 py-2 font-mono text-base font-medium text-justify bg-gray-900 rounded-lg" style="color:526777">
             <button>#custom-words</button>
             <button>
                 <div class="flex items-center"><img src="{{ asset('assets/img/clock.png') }}" alt="time"
@@ -33,20 +33,29 @@
                     <span class="ml-2">words</span>
             </button>
             <div class="flex items-center ml-4">
-                <button class="ml-2">15</button>
-                <button class="ml-2">30</button>
-                <button class="ml-2" style="color: #2ABA86">60</button>
-                <button class="ml-2">120</button>
+                <button class="ml-2" id="btn-15" onclick="changeColor(this)">15</button>
+                <button class="ml-2" id="btn-30" onclick="changeColor(this)">30</button>
+                <button class="ml-2" id="btn-60" onclick="changeColor(this)" style="color: #2ABA86">60</button>
+                <button class="ml-2" id="btn-120" onclick="changeColor(this)">120</button>
             </div>
+            <script>
+                function changeColor(button) {
+                  var buttons = document.getElementsByTagName('button');
+                  for (var i = 0; i < buttons.length; i++) {
+                    buttons[i].style.color = '#526777';
+                  }
+                  button.style.color = '#2ABA86';
+                }
+              </script>
         </div>
     </div>
 
-    <button class="flex items-center justify-center mx-auto">
+    <button id="language-btn" class="flex items-center justify-center mx-auto">
         <img src="{{ asset('assets/img/earth.png') }}" alt="" class="mr-2" width="18" height="16"
             style="filter: invert(38%) sepia(13%) saturate(808%) hue-rotate(164deg) brightness(95%) contrast(85%);">
         <span class="ml-1 font-mono text-sm font-medium text-gray-600">Indonesia</span>
     </button>
-    {{-- Awal type --}}
+
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <div class="flex items-center justify-center mx-auto">
         <div class="font-mono wrapper">
@@ -68,6 +77,10 @@
                         <p>CPM:</p>
                         <span>0</span>
                     </li>
+                    <li class="accuracy">
+                        <p>Accuracy:</p>
+                        <span>0%</span>
+                    </li>
                 </ul>
             </div>
             <input type="text" class="input-field">
@@ -76,13 +89,14 @@
                     <p></p>
                 </div>
                 <div class="flex justify-center mx-20 mt-8">
-                        <button class="refresh"><img src="{{ asset('assets/img/refresh-arrow.png') }}" alt=""
-                                style="filter: invert(62%) sepia(49%) saturate(637%) hue-rotate(107deg) brightness(90%) contrast(82%);"
-                                width="35" height="35"></button>
+                    <button class="refresh"><img src="{{ asset('assets/img/refresh-arrow.png') }}" alt=""
+                            style="filter: invert(62%) sepia(49%) saturate(637%) hue-rotate(107deg) brightness(90%) contrast(82%);"
+                            width="35" height="35"></button>
                 </div>
             </div>
         </div>
     </div>
+
     {{-- akhir type --}}
     <!-- akhir typing section -->
     <script src={{ asset('js/paragraphs.js') }}></script>
