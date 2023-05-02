5bd073967e827b2a239c77fe5dfe3965959d7318
Fanul1 <fanul@mhs.unsyiah.ac.id>
2023-05-02 15:15:58 +0700

    fix: fix route

--- CapyType/routes/web.php
+++ CapyType/routes/web.php
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('index');
+});
 
-
-Route::get('type', function () {
+Route::get('/type', function () {
     return view('typing');
 });
 
